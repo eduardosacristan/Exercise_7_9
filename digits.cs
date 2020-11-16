d3b38b9c2528fbc466a0df48f7e6793a6f06c08e
eduardosacristan <73886009+eduardosacristan@users.noreply.github.com>
2020-11-16 10:35:34 +0100

    source improvement
    I've modified the code by improving the spacing and formatting.

--- digits.cs
+++ digits.cs
@@ -4,34 +4,26 @@ using System;
 
 class digits
 {
- static void Main()
- {
-
-     int a = 0, b = 0, c = 0, d = 0;
-      Console.Write("Insert a number: ");
+    static void Main()
+    {
+        int a = 0, b = 0, c = 0, d = 0;
+        Console.Write("Insert a number: ");
       
-      string e = Console.ReadLine();
-     while(e!="end"){
-int input = Convert.ToInt32(e);
-       if(input != 0)
-{
-       if(input/10 == 0)
-           a ++; 
-       else 
-       if (input/100 == 0)
-		b ++;  
-        else
-        if (input/1000 == 0)
-        c ++;  
-        else
-           d++;  
+        string e = Console.ReadLine();
+        while(e!="end")
+        {
+            int input = Convert.ToInt32(e);
+            if(input != 0)
+            {
+                if(input/10 == 0) a++; 
+                else if (input/100 == 0) b ++;  
+                else if (input/1000 == 0) c ++;  
+                else d++;  
+            }
+        e = Console.ReadLine();
         }
-      e = Console.ReadLine();
-}
 
-Console.WriteLine("one: {0} two: {1} three: {2} more: {3}", a, b, c, d); 
-
-
- }
+    Console.WriteLine("one: {0} two: {1} three: {2} more: {3}", a, b, c, d);
+    }
 }
 
