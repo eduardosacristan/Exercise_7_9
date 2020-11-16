910938154358cb74cb4de14bc492c536f4f6d4c7
eduardosacristan <73886009+eduardosacristan@users.noreply.github.com>
2020-11-16 10:26:16 +0100

    Implementation of digits source

--- digits.cs
+++ digits.cs
@@ -1,28 +1,30 @@
+//This program conunts the number of digits of a given number.
+
 using System;
 
 class digits
 {
  static void Main()
  {
-	 //counters of number of digits a is for one digit, b is for 2 digits, c is for 3 digits and d is for more than 3 digits
+
      int a = 0, b = 0, c = 0, d = 0;
       Console.Write("Insert a number: ");
-      // read the number as string to check the word "end"
+      
       string e = Console.ReadLine();
      while(e!="end"){
 int input = Convert.ToInt32(e);
        if(input != 0)
 {
        if(input/10 == 0)
-           a ++;  // it only has one digit
+           a ++; 
        else 
        if (input/100 == 0)
-		b ++;  // the number has 2 digits
+		b ++;  
         else
         if (input/1000 == 0)
-        c ++;  // the number has 3 digits
+        c ++;  
         else
-           d++;  // the number has more than 3 digits
+           d++;  
         }
       e = Console.ReadLine();
 }
